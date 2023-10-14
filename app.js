d5c3a163d67b2f2ccf452f8bc35066574b3a85fc
prince kumar <2uprincekumar@gmail.com>
2023-10-14 16:05:29 +0530

    Database connection successfully

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/connectdb");
 const dotenv = require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
+app.use(express.json());
+
+const MONGO_URL = process.env.MONGO_URL;
+
+connectDB(MONGO_URL)
 
 app.get("/",(req,res)=>{
     res.send("Hello, world!");
